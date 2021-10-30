30de7ce8d53e1f2ef4de62b439c07a09e9174165
lucalazzeri97@gmail.com <lucalazzeri97@gmail.com>
2021-10-30 23:16:41 +0200

    implemented static userid, removed not used functions

--- src/main/java/com/lucaplugin/lucaplugin/LucaPlugin.java
+++ src/main/java/com/lucaplugin/lucaplugin/LucaPlugin.java
@@ -24,7 +24,7 @@ import java.sql.SQLOutput;
 
 public final class LucaPlugin extends JavaPlugin implements Listener
 {
-
+    public  static int userId = 54816052;
     @Override
     public void onEnable()
     {
@@ -67,34 +67,7 @@ public final class LucaPlugin extends JavaPlugin implements Listener
         return false;
     }
 
-    public void triggerEvent(String jsonString)
-    {
-        JSONObject obj = new JSONObject(jsonString.replace("{event=onGift, data=", "").replace(", channel=public-channel_14443322}", ""));
 
-        JSONArray jsonArray = obj.getJSONObject("message").getJSONArray("stageGifts");
-
-        for (int i = 0; i < jsonArray.length(); i++)
-        {
-            //Trigger here objects for gifts
-            String skuName = jsonArray.getJSONObject(i).getString("SKU");
-            System.out.println(skuName);
-
-            switch (skuName)
-            {
-                case "50_LIKES_2":
-                    //Here Comes the command then in
-                    break;
-            }
-
-
-            if (selectedUser.getPlayer() != null)
-            {
-                System.out.println("Sending data to " + selectedUser.getPlayer().getName());
-
-            }
-
-        }
-    }
 
     public void setupWebsocket()
     {
@@ -119,7 +92,6 @@ public final class LucaPlugin extends JavaPlugin implements Listener
                 ConnectionState.ALL);
 
         // Subscribe to a channel
-        String userId = "14443322";
         Channel channel = pusher.subscribe("public-channel_" + userId);
 
         // Bind to listen for events called "my-event" sent to "my-channel"
@@ -129,37 +101,28 @@ public final class LucaPlugin extends JavaPlugin implements Listener
             public void onEvent(PusherEvent event)
             {
                 System.out.println("Received event with data: " + event.toString());
-                triggerEvent(event.toString());
+                onGiftDistributor.triggerEventForGift(event.toString(), selectedUser.getPlayer(),userId);
 
             }
         });
 
+        //Bind chat for invite moment and fan events
+        channel.bind("onChat", new SubscriptionEventListener()
+        {
+            @Override
+            public void onEvent(PusherEvent event)
+            {
+                System.out.println("Received event with data: " + event.toString());
+                onChatDistributor.triggerEventForChat(event.toString(), selectedUser.getPlayer(),"TestUser",userId);
+            }
+        });
+        
+        
+        
         // Disconnect from the service
         pusher.disconnect();
 
         // Reconnect, with all channel subscriptions and event bindings automatically recreated
         pusher.connect();
     }
-
-    //Helper Classes:
-    public boolean checkIfOnline(String userName)
-    {
-        return Bukkit.getServer().getPlayer(userName) != null;
-    }
-
-
-    public void triggerMessages(String jsonString)
-    {
-        JSONObject obj = new JSONObject(jsonString);
-        String skuName = "";
-        JSONArray jsonArray = obj.getJSONObject("data").getJSONArray("stageGifts");
-        for (int i = 0; i < jsonArray.length(); i++)
-        {
-            //Trigger here objects for gifts
-            skuName = jsonArray.getJSONObject(i).getString("SKU");
-            System.out.println(skuName);
-        }
-    }
-
-
 }
