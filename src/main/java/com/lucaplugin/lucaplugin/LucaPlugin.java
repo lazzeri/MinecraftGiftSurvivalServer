572b66d4e4c58fc8e01f73c287b7abd3b6152ded
lucalazzeri97@gmail.com <lucalazzeri97@gmail.com>
2023-07-08 10:28:42 +0200

    add websocket connection

--- src/main/java/com/lucaplugin/lucaplugin/LucaPlugin.java
+++ src/main/java/com/lucaplugin/lucaplugin/LucaPlugin.java
@@ -1,5 +1,13 @@
 package com.lucaplugin.lucaplugin;
 
+import com.pusher.client.Pusher;
+import com.pusher.client.PusherOptions;
+import com.pusher.client.channel.Channel;
+import com.pusher.client.channel.PusherEvent;
+import com.pusher.client.channel.SubscriptionEventListener;
+import com.pusher.client.connection.ConnectionEventListener;
+import com.pusher.client.connection.ConnectionState;
+import com.pusher.client.connection.ConnectionStateChange;
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.Location;
@@ -359,5 +367,59 @@ public final class LucaPlugin extends JavaPlugin implements Listener
                 eventHandlerObj.tntRain(player, "TestName", this);
             }
         }
+    }
+
+    public void setupWebsocket(int userId)
+    {
+        PusherOptions options = new PusherOptions().setCluster("mt1");
+        Pusher pusher = new Pusher("42a54e2785b3c81ee7b3", options);
+
+        pusher.connect(new ConnectionEventListener()
+                       {
+                           @Override
+                           public void onConnectionStateChange(ConnectionStateChange change)
+                           {
+                               System.out.println("State changed to " + change.getCurrentState() +
+                                       " from " + change.getPreviousState());
+                           }
+
+                           @Override
+                           public void onError(String message, String code, Exception e)
+                           {
+                               System.out.println("There was a problem connecting!");
+                           }
+                       },
+                ConnectionState.ALL);
+
+        // Subscribe to a channel
+        Channel channel = pusher.subscribe("public-channel_" + userId);
+
+        // Bind to listen for events called "my-event" sent to "my-channel"
+        channel.bind("onGift", new SubscriptionEventListener()
+        {
+            @Override
+            public void onEvent(PusherEvent event)
+            {
+                System.out.println("Received event with data: " + event.toString());
+                onGiftDistributor.triggerEventForGift(event.toString(), selectedUser.getPlayer(), userId);
+            }
+        });
+
+        //Bind chat for invite moment and fan events
+        channel.bind("onChat", new SubscriptionEventListener()
+        {
+            @Override
+            public void onEvent(PusherEvent event)
+            {
+                System.out.println("Received event with data: " + event.toString());
+                onChatDistributor.triggerEventForChat(event.toString(), selectedUser.getPlayer(), "TestUser", userId);
+            }
+        });
+
+        // Disconnect from the service
+        pusher.disconnect();
+
+        // Reconnect, with all channel subscriptions and event bindings automatically recreated
+        pusher.connect();
     }
 }
